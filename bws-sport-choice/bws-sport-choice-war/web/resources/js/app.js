bd25f2128690f6116e6951d8c79f3325eba10c09
Lisa Lichtenfels <lis-lich@web.de>
2018-10-04 13:39:31 +0200

    Mockups angelegt

--- bws-sport-choice/bws-sport-choice-war/web/resources/js/app.js
+++ bws-sport-choice/bws-sport-choice-war/web/resources/js/app.js
@@ -1,3 +1,34 @@
-document.getElementById("menu").onclick(function(){
-    document.getElementById("togglebar").style.cssText("visibility : visible;");
+$(document).ready(function(){
+    $("#logo").attr("src" , "../imgs/logo.svg");
+    
+    $("#menu").click(function(){
+        $("#togglebar").toggle(function(){
+            $("#togglebar").addClass("hide");
+        }, function(){
+            $("#togglebar").removeClass("hide");
+        });
+        
+    });
+    
+    $("#UWFEins").change(function () {
+        if ($("#UWFEins").is(" :checked")) {
+            $("#UWFZwei").attr("disabled", true);
+            $("#UWFDrei").attr("disabled", true);
+        }
+    });
+    
+    $("#UWFZwei").change(function () {
+        if ($("#UWFZwei").is(" :checked")) {
+            $("#UWFEins").attr("disabled", true);
+            $("#UWFDrei").attr("disabled", true);
+        }
+    });
+    
+    $("#UWFDrei").change(function () {
+        if ($("#UWFDrei").is(" :checked")) {
+            $("#UWFZwei").attr("disabled", true);
+            $("#UWFEins").attr("disabled", true);
+        }
+    });
+    
 });
