40e283a8ca601968749f45fbf135a10c301cdcab
Lisa Lichtenfels <lis-lich@web.de>
2018-11-21 09:44:20 +0100

    Kommentare in DateConverter hinzugefügt

--- bws-sport-choice/bws-sport-choice-war/src/java/de/bws/converter/DateConverter.java
+++ bws-sport-choice/bws-sport-choice-war/src/java/de/bws/converter/DateConverter.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.bws.converter;
 
-import javax.faces.convert.Converter;
 import javax.faces.convert.DateTimeConverter;
 import javax.faces.convert.FacesConverter;
 
 /**
- *
+ * Diese Klasse dient als Konverter der Eingabedaten zum gewünschten Datumsformat.
+ * 
  * @author Lisa
  */
 @FacesConverter("DateConverter")
